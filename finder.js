8a4ed95f19d98038b13d35a8dc3f510e62dfc915
brezhart <brezhart@gmail.com>
2021-02-10 16:45:24 +0300

    upd

--- finder.js
+++ finder.js
@@ -22,7 +22,6 @@ if (true) { //Date.now() < 1583010000000
 
         //
         var date = new Date(info.timestamp);
-        console.log(info.timestamp);
         let dateCont = document.getElementById("dateCont");
         dateCont.innerText = `${toFull(date.getHours())}:${toFull(date.getMinutes())}  ${toFull(date.getDate())}/${toFull(date.getMonth()+1)}/${toFull(date.getFullYear())} (D/M/Y)`;
 
@@ -45,13 +44,12 @@ if (true) { //Date.now() < 1583010000000
                     name = value.label;
                 }
             }
-            console.log("End: ", name);
             target = name;
             if (name !== false ){
-                console.log(name);
+                let haveShops = false;
                 for (let [key, value] of Object.entries(unpackedCords)) {
                     if (value.label.toUpperCase() == target.toUpperCase()) {
-                        if (i == 0) {
+                        if (i == 1) {
                             targetPlace.innerText = target;
                             cantFind.innerText = "";
                             table.style.display = 'table';
@@ -93,10 +91,59 @@ if (true) { //Date.now() < 1583010000000
                             newTr.appendChild(newTd);
                             newThread.appendChild(newTr);
                             table.appendChild(newThread);
+                        } else {
+                            haveShops = true;
                         }
                         i++;
 
+                    }
+                }
+                if (haveShops){
 
+                    let i = 1;
+                    let newThread = document.createElement("thead");
+                    let newTr = document.createElement('tr');
+                    let newTh = document.createElement('th');
+                    newTh.textContent = "   Shops ";
+                    newTr.appendChild(newTh);
+                    newTh = document.createElement('th');
+                    newTh.textContent = "  x  ";
+                    newTr.appendChild(newTh);
+                    newTh = document.createElement('th');
+                    newTh.textContent = "  z  ";
+                    newTr.appendChild(newTh);
+                    newThread.appendChild(newTr);
+                    newTh = document.createElement('th');
+                    newTh.textContent = "link";
+                    newTr.appendChild(newTh);
+                    newThread.appendChild(newTr);
+                    table.appendChild(newThread);
+
+
+                    for (let [key, value] of Object.entries(unpackedCords)) {
+                        if (value.label.toUpperCase() == target.toUpperCase()) {
+                            if (key.substring(key.length-4, key.length) == "Shop") {
+                                let newThread = document.createElement("thead");
+                                let newTr = document.createElement('tr');
+                                let newTd = document.createElement('td');
+                                newTd.textContent = i;
+                                newTr.appendChild(newTd);
+                                newTd = document.createElement('td');
+                                newTd.textContent = value.x[0];
+                                newTr.appendChild(newTd);
+                                newTd = document.createElement('td');
+                                newTd.textContent = value.z[0];
+                                newTr.appendChild(newTd);
+                                newTd = document.createElement('td');
+                                newTd.textContent = 'On map';
+                                newTd.style.color = "#E27D60";
+                                newTd.setAttribute('onclick', "openMap(" + value.x[0] + "," + value.z[0] + "," + server + ")");
+                                newTr.appendChild(newTd);
+                                newThread.appendChild(newTr);
+                                table.appendChild(newThread);
+                                i++;
+                            }
+                        }
                     }
                 }
             } else {
